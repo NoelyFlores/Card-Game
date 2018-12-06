3e25468dd42a7f8c64695333948b1d963ac44f28
noelyGitHub <noely.alis209@gmail.com>
2018-12-05 18:26:41 -0600

    variables en ingles

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,50 +1,66 @@
 <template>
     <v-card>
-        <v-card-text>Mierda {{number}}</v-card-text>
+        <v-card-text>Nivel {{number}}</v-card-text>
 				      
         <div id="app">
-  <v-app id="inspire">
-    <v-layout justify-center>
-      <v-flex xs12 sm12>  
-        <v-card class="container-card-vue">
-          <v-container
-            fluid
-            grid-list-md
-          >
-            <v-layout row wrap>
-              <v-flex
-                 v-for="(item, index) in items" :key="index"								
-								 @click="acierta(item.number,item.type, item.state,index)"
-              >
-                <v-card>
-                  <v-img
-                    height="250px"
-                    width="100%"
-										
+          <v-app id="inspire">
+            <v-layout justify-center>
+              <v-flex xs12 sm12>  
+                <v-card class="container-card-vue">
+                  <v-container
+                    fluid
+                    grid-list-md
                   >
-                    <v-flex xs12 sm12>
-                      <v-card class="container-vue" v-bind:class="{'atras': !items[index].state}" >
-                        {{index}}
-                        <v-container
-                        fluid
-                        grid-list-md												                  
-                        >
-                        <v-layout row wrap class="layout-vue" ><!--   -->
-                          <v-icon v-for="i in item.number" :key="i" v-if="item.state" large color="red darken-2"  class="icon">{{item.type}}</v-icon>
-                        </v-layout>
-                       </v-container>
-                      </v-card>
-                    </v-flex>
-                  </v-img>
+                    <v-layout row wrap>
+                      <v-flex
+                        v-for="(item, index) in items" :key="index"								
+                        @click="choose(item.number,item.type, item.state,index)"
+                      >
+                        <v-card>
+                          <v-img
+                            height="232px"
+                            width="100%"										
+                          >
+                            <v-flex xs12 sm12>
+                              <v-card class="container-vue" v-bind:class="{'upside': !items[index].state}"><!-- :id='`successful${index}`' -->
+                                <span class="numero" v-if="items[index].state"  :class="`successful${index}`">{{item.number}}</span>
+                                  <v-container
+                                  fluid
+                                  grid-list-md												                  
+                                  >
+                                  <v-layout row wrap class="layout-vue" ><!--   -->
+                                    <v-icon v-for="i in item.number" :key="i" v-if="item.state" large color="red darken-2" :class="`icon successful${index}`" >{{item.type}}</v-icon>
+                                  </v-layout>
+                                </v-container>                   
+                              </v-card>
+                            </v-flex>
+                          </v-img>
+                        </v-card>
+                      </v-flex>              
+                    </v-layout>
+                  </v-container>
                 </v-card>
               </v-flex>
-              
             </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-app>
+          </v-app>
+          <div id="app">
+            <v-app id="inspire">
+              <v-layout row justify-center>
+                <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+                  <v-btn slot="activator" color="primary" dark>Open Dialog</v-btn>
+                  <v-card class="container-modal">
+                    <v-btn icon dark color="red" @click="dialog = false">
+                      <v-icon>close</v-icon>
+                    </v-btn>
+                    <v-divider></v-divider>
+                    <v-btn icon dark color="blue" @click="dialog = false">
+                      <v-icon>close</v-icon>
+                    </v-btn>
+                  </v-card>
+                </v-dialog>
+              </v-layout>
+            </v-app>
+          </div>
 </div>
     </v-card>
 </template>
@@ -54,106 +70,86 @@ export default {
   data() {
     return {
       number: 0,
-      min: 0,
-			max: 8,
-			cantCard: 10,
-			typeCard: ['favorite', 'toys', 'edit','home'],
+			numberLetters: 8,
+			typeLetter: ['favorite', 'toys', 'edit','home'],
       items: [],
-			acierto: false,
-			valueOne: 0,
-			count: 0,
-			ocultarAcierto: false,
-			valueType: '',
-			cardState: 0
+      temp: [],
+      dialog: true,
+      nivel: []
     };
   },
   created() {
-		//this.items = this.generarItem()
-		//this.aleatorio = this.aleatorioUnico(this.items.length);
-		
-    const data= JSON.stringify(this.generarCartasAleatorias(), null, this.cantCard)
-    this.items = JSON.parse(data) 
-		console.log(this.items)
+		const data= JSON.stringify(this.generateRandomLetters(), null, this.numberLetters)
+    this.items = JSON.parse(data)
 	},
   methods: {
 		// genero las cartas de 4 tipos hasta el numero 10
-		generarItem(){
+		generateItem(){
 			const temp = []
-				this.typeCard.forEach(element => {
-				for (let i = 0; i < 10; i++) {
-					 temp.push({type: element, number: i+1, state: false})
-				}
+			this.typeLetter.forEach(element => {
+        for (let i = 0; i < 10; i++) {
+          temp.push({type: element, number: i+1, state: false})
+        }
 			});
 			return temp
 		},
 		//genero numeros aleatorios :(
-    aleatorioUnico(cantidad) {
+    randomOnly(cantidad) {
       let array = [];
       for (let i = 0; i < cantidad; i++) {
-        //array=insertarEn(array,i,Math.random()*(cantidad+1) )
-        const posición = Math.random() * (cantidad + 1);
-        const inicio = array.slice(0, posición); //captura el valor del lado izquierdo
-        const medio = i; // indice del array actual
-        const fin = array.slice(posición); // array del lado i
-        array = inicio.concat(medio).concat(fin);
+        const position = Math.random() * (cantidad + 1);
+        array = array.slice(0, position).concat(i).concat(array.slice(position));
 			}
       return array;
 		},
-		// modifico el items con los numeros aleatorios
-		// tengo items y numers aleatorios
-		generarCartasAleatorias(){
+		generateRandomLetters(){
 			//voy a reccorrer los numeros aleatorios solo hasta la mitad del numero de cartas solicitadas
-			const listCard = this.generarItem()//length 40
-			const cantTiros = this.aleatorioUnico(listCard.length).slice(0, this.cantCard/2)// length num escogido
-			const doblelistCard = cantTiros.concat(cantTiros)// 12
-			const aleatorioListCartas = this.aleatorioUnico(doblelistCard.length) //posicion
-			aleatorioListCartas.map((element, index) => {
-				 aleatorioListCartas[index] = doblelistCard[element]
+			const listLetters = this.generateItem()// Array de objetos con información de la carta
+			const cantTiros = this.randomOnly(listLetters.length).slice(0, this.numberLetters/2)// numero de cartas en aleatorio a mostrar
+			const doublelistLetters = cantTiros.concat(cantTiros)// lista con el doble de cartas 
+			const randomListCartas = this.randomOnly(doublelistLetters.length) // doble lista de cartas en aleatorio
+			randomListCartas.map((element, index) => {
+				 randomListCartas[index] = doublelistLetters[element]
 			});
-			aleatorioListCartas.map((position, index) => {			
-				aleatorioListCartas[index]=listCard[position]
+			randomListCartas.map((position, index) => {			
+				randomListCartas[index]=listLetters[position]
 			})
-			return aleatorioListCartas
+			return randomListCartas
 		},
-    acierta(value, valType,st, index){
-      
-      console.log(this.items[index], index)
-      console.log(this.items)
-      
+    choose(value, valType,st, index){
       if(this.items[index].state !== true){
         this.items[index].state = true
-        this.count++
-        const cardAcertada = this.cardState
-        this.cardState = index
-
-        const primerAcierto = this.valueOne
-        this.valueOne = value
-
-        const type = this.valueType
-        this.valueType = valType
-
-			if(this.count === 2){
-					if(primerAcierto === this.valueOne && type === this.valueType){											
-						console.log('acertaste')
-						this.ocultarAcierto = true
-						}else{
-							const self = this;
-							setTimeout(function () {
-								console.log(self.cardState, cardAcertada)
-								self.items[index].state = false
-								self.items[cardAcertada].state = false
-							},500)						
-					}
-					this.count = 0
-					console.log(this.valueOne)
-			}	
-				}
-			/*no crean que dire lo mismo que todos -que he cometido errores y no me arrepiento, pues 
-			yo digo que durante toda mi puta vida me arrepentire de haber perdido mi tiempo contigo y haber dejado pasar oportunidades
-			creendo que tu eres el mejor, me arrepiento de haberlo entregado todo y haber intentando una y otra vez creendo que esto iba funcionar
-			No es rencor ni desahogo pero los errores se tienen que pagar y yo lo hare toda mi vida por que desperdicie todo!!!*/
-			
-		}
+        this.temp.push({numero: value, figura: valType, posicion: index})  // array de de 2 aciertos a comparar   
+        if(this.temp.length === 2){
+          this.timeotu()
+          .then(() => {
+            this.temp = []
+            })
+          }	
+			}		
+    },
+    timeotu(){
+      return new Promise((resolve, reject) => {
+        if(this.temp[0].numero === this.temp[1].numero && this.temp[0].figura === this.temp[1].figura) {
+          const listIcons = document.getElementsByClassName(`successful${this.temp[0].posicion}`)
+          const listIcons2 = document.getElementsByClassName(`successful${this.temp[1].posicion}`)          
+          setTimeout(function () {
+            for (let index = 0; index <= 10; index++) {// numero maximo de figuras en la carta
+              listIcons[index]?listIcons[index].style.display = 'none': ''              
+              listIcons2[index]?listIcons2[index].style.display = 'none': ''
+            }
+					},200)          
+          resolve()
+        } else{
+          const self = this;
+					setTimeout(function () {
+					  self.items[self.temp[0].posicion].state = false
+            self.items[self.temp[1].posicion].state = false
+            resolve()
+					},500)
+        }        
+      })
+    }
   }
 };
 </script>
@@ -161,17 +157,6 @@ export default {
 .card-box,
 .card-conten {
   display: flex;
-}
-.card-content {
-  border: 1px solid red;
-  width: 200px;
-  height: 200px;
-}
-.card-content-all {
-  margin: auto;
-}
-.layaut {
-  border: 1px green;
 }
 .icon {
   width: auto;
@@ -192,21 +177,38 @@ export default {
   display: flex;
 }
 .container-vue {
-  height: 242px;
+  height: 222px;
   display: flex;
   cursor: pointer;
 }
-.atras {
+.upside {
   background: url(https://scontent.flim6-1.fna.fbcdn.net/v/t1.15752-9/47230104_329031931254301_7159184941643726848_n.jpg?_nc_cat=103&_nc_eui2=AeGUJboYC0sL5RcmcPLa07W8yO92axxC7BzTimjtPcUzdWXJ_uhnA4--4SiSWe2nxMtMpcZEIk_4p9VEZyA9GmIzlJk_M5ew7opD3vGq9qDOxw&_nc_ht=scontent.flim6-1.fna&oh=961516c674aa2bf3adc94cb43b562d13&oe=5CA94D46)
     no-repeat;
 }
-
+.successful{
+  background: #e91e63 !important
+}
 .container-vue:hover {
   box-shadow: 0px 1px 8px 1px #e91e63;
 }
 .container-card-vue {
-  width: 800px;
+  width: 735px;
   margin: auto;
+}
+.numero{
+  color: #17171b;
+  font-size: 30px;
+  padding: 0px 0px 0px 4px;
+  margin-top: -5px;
+  font-weight: 800;
+  position: absolute;
+}
+.container-modal{
+  background: url(https://images.pexels.com/photos/297507/pexels-photo-297507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat;
+  height: 100vh;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
 }
 </style>
 
